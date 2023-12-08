2a6847baf8e347d71ccd6c58fdc7d7ad642ec083
manhathaway <alex.mannoia@gmail.com>
2023-12-07 21:10:48 -0800

    3.4

---
+++ 3.4 - Middleware/index4.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  res.send("<h1>Your band name is:</h1>" + req.body.street + req.body.pet);
+})
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+})
