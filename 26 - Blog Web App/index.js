aae55883322152915afd1657f646490665b830c9
manhathaway <alex.mannoia@gmail.com>
2023-12-12 16:54:51 -0800

    26 (Progress)

---
+++ 26 - Blog Web App/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("welcome.ejs");
+});
+
+app.post("/main", (req, res) => {
+    res.render("main.ejs", {
+        name: req.body.name
+    });
+});
+
+app.post("/blogged", (req, res) => {
+    res.render("main.ejs", {
+        blog: req.body.blogPost
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Now running on ${port}.`);
+});
