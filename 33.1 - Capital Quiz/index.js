0289b404b47d60d3f294d02319a755d3779de94d
manhathaway <alex.mannoia@gmail.com>
2024-01-27 16:51:48 -0800

    33.1 - 33.2, 33.3 Started

---
+++ 33.1 - Capital Quiz/index.js
@@ -0,0 +1,69 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+let quiz = [];
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "webdevlife",
+  port: 5432,
+});
+
+db.connect();
+
+db.query("SELECT * FROM capitals", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    console.log("Query executed successfully.");
+    quiz = res.rows;
+  }
+  db.end();
+});
+
+let totalCorrect = 0;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let currentQuestion = {};
+
+// GET home page
+app.get("/", async (req, res) => {
+  totalCorrect = 0;
+  await nextQuestion();
+  res.render("index.ejs", { question: currentQuestion });
+});
+
+// POST a new post
+app.post("/submit", (req, res) => {
+  let answer = req.body.answer.trim();
+  let isCorrect;
+  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+    totalCorrect++;
+    isCorrect = true;
+  } else {
+    isCorrect = false;
+  };
+
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    wasCorrect: isCorrect,
+    totalScore: totalCorrect,
+  });
+});
+
+async function nextQuestion() {
+  currentQuestion = quiz[Math.floor(Math.random() * quiz.length)];
+}
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
