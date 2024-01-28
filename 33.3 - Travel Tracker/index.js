c5bafc34ed6a60c51390f520322545f7f9abd8fa
manhathaway <132020906+manhathaway@users.noreply.github.com>
2024-01-28 12:17:02 -0800

    33.3 (INP)

--- 33.3 - Travel Tracker/index.js
+++ 33.3 - Travel Tracker/index.js
@@ -1,14 +1,35 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from 'pg';
 
 const app = express();
 const port = 3000;
+let countries = []
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const db = new pg.Client({
+  user: 'postgres',
+  password: 'webdevlife',
+  database: 'world',
+  host: 'localhost',
+  port: 5432
+});
+
 app.get("/", async (req, res) => {
-  //Write your code here.
+  db.connect();
+  const result = await db.query('SELECT * FROM visited_countries');
+
+  res.render("index.ejs", {
+    total: result.rows.length,
+    countries: result.rows.map(country => country.country_code)
+  })
+  db.end();
+});
+
+app.post('/add', async (req, res) => {
+  console.log(req.body.country);
 });
 
 app.listen(port, () => {
